26d73f481c678d3354a24ccba704768ab4786d49
Ahmed <ahmed.jehairan@gmail.com>
2017-11-20 20:29:52 -0600

    sourced makesymlinks file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,6 +2,7 @@
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Slightly edited version of https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
 ############################
 
 ########## Variables
