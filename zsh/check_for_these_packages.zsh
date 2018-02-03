570dbc421bea6fdca6a02274f7994bdc5b83877f
Ahmed Jehairan <ahmed.jehairan@gmail.com>
2018-02-03 10:11:00 -0600

    fix(zshrc): put everything back in one file -- .zshrc. This is because of circular sourcing in the files

--- zsh/check_for_these_packages.zsh
+++
@@ -1,9 +0,0 @@
-#! /bin/zsh
-if ($new_installation);
-then
-    local progs="fortune cowsay lolcat colordiff emacs git nvim nyaovim vimpager pacaur man2html fzy autojump"
-    progs_arr=(${(ps: :)${progs}})
-    for prog in $progs_arr; do
-        if (( $+commands[$prog] )); then; else echo "$prog missing"; fi
-    done;
-fi
--- zsh/fedora.zsh
+++
@@ -1,1 +0,0 @@
-zplug "plugins/fedora", from:oh-my-zsh
